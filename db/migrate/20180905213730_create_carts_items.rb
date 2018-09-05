f0caf41550dadb6238107809bb460a1695d1f14c
gusarov2k <motoface@yandex.ru>
2018-09-06 00:42:13 +0300

    add code in migration create_carts_items

--- db/migrate/20180905213730_create_carts_items.rb
+++ db/migrate/20180905213730_create_carts_items.rb
@@ -1,6 +1,19 @@
 class CreateCartsItems < ActiveRecord::Migration
-  def change
-    create_table :carts_items do |t|
+  def self.up
+    create_table :carts_items, id false do |t|
+    	# t.references :cart
+    	t.integer :cart_id
+    	# t.references :item
+    	t.integer :item_id
     end
+
+    # add index 
+    add_index :carts_items, [:item_id, :cart_id]
+		add_index :carts_items, [:cart_id, :item_id]
+  end
+
+  # if rollback this migration method delete 
+  def self.down
+  	drop_table :carts_items
   end
 end
