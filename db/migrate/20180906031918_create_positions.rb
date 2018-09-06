d91332515c203e7fba752b24d0faea552f225fb0
gusarov2k <motoface@yandex.ru>
2018-09-06 06:24:00 +0300

    add column in migration position

--- db/migrate/20180906031918_create_positions.rb
+++ db/migrate/20180906031918_create_positions.rb
@@ -2,7 +2,13 @@ class CreatePositions < ActiveRecord::Migration
   def change
     create_table :positions do |t|
 
+      t.integer :item_id
+      t.integer :busket_id
+      t.integer :quantity, default: 1
       t.timestamps null: false
     end
+
+    add_index :positions, [:item_id, :busket_id]
+    add_index :positions, [:busket_id, :item_id]
   end
 end
