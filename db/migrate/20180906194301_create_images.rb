b76b28e49d6bae784dd69f31138965df1fb3d2b2
gusarov2k <motoface@yandex.ru>
2018-09-06 22:45:42 +0300

    add column in migration image

--- db/migrate/20180906194301_create_images.rb
+++ db/migrate/20180906194301_create_images.rb
@@ -2,7 +2,9 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
 
-      t.timestamps null: false
+    	t.integer 	:imageable_id
+    	t.string 	:imageable_type
+      	t.timestamps null: false
     end
   end
 end
