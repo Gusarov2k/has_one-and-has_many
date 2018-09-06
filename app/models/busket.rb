ebed8b7de664e15706cd39036d07dbe2952dd3a3
gusarov2k <motoface@yandex.ru>
2018-09-06 07:05:22 +0300

    add method in model basket

--- app/models/busket.rb
+++ app/models/busket.rb
@@ -4,4 +4,13 @@ class Busket < ActiveRecord::Base
 	# has_many through
 	has_many :positions
 	has_many :items, through: :positions
+
+# add castom method add item quantity in busket
+	def add_item(i)
+		if items.include?(i)
+			positions.where(item_id: i.id).first.increment!(:quantity)
+		else
+			positions.create(item: i, quantity: 1)
+		end
+	end
 end
