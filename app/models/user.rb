1df8e38514e0d5231cf1769080276e485ab252cf
gusarov2k <motoface@yandex.ru>
2018-09-06 22:56:20 +0300

    add associacion in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	has_one :busket
 	# if delete row user to delete all his row orders, for it have add code dependent: :destroy
 	has_many :orders, dependent: :destroy
+	# add polymorf associaons
+	has_many :images, as: :imageable
 end
