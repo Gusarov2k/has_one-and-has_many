f380bcffda460172232dbb1487495b8c7110280e
gusarov2k <motoface@yandex.ru>
2018-09-08 11:50:49 +0300

    create columns in employees

--- db/migrate/20180908084622_add_employees.rb
+++ db/migrate/20180908084622_add_employees.rb
@@ -1,4 +1,11 @@
 class AddEmployees < ActiveRecord::Migration
   def change
+  	create_table :employees do |t|
+	  	t.string 	:subtipe
+	  	t.string	:first_name
+	  	t.string	:last_name
+	  	t.string	:salery
+	  	t.timestamps
+  	end
   end
 end
