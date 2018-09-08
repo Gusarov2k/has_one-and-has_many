b4351d126c41c5d55d4c01a0b6b67670f7742f03
gusarov2k <motoface@yandex.ru>
2018-09-08 12:04:36 +0300

    add code in  migration programmers

--- db/migrate/20180908090230_create_programmers.rb
+++ db/migrate/20180908090230_create_programmers.rb
@@ -1,6 +1,7 @@
 class CreateProgrammers < ActiveRecord::Migration
   def change
-    create_table :programmers do |t|
+    create_table :programmers, :inherits => :employee  do |t|
+    	t.string :programmers_column
     end
   end
 end
